cf2af156d0325e033226c6b6e972c9636615f786
Ishita Namdeo <72147337+AMANOZAKO@users.noreply.github.com>
2022-04-20 21:04:28 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,22 @@
+a = "Its Ishita here"
+b = "2"     # b is a string
+c = 2       # c is a number
+
+x, y = 10, 5
+print("x + y = ", x + y)     # addition
+print("x - y = ", x - y)     # subtraction
+print("x * y = ", x * y)     # multiplication
+print("x / y = ", x / y)     # division
+print("x^y = ", x ** y)      # power
+print("remainder when x divides y = ", x % y)     # modulus
+print("x // y = ", x // y)     # floor division
+
+x, y = 3.143217, 8.20456
+print("x + y = ", x + y)     # addition
+print("x - y = ", x - y)     # subtraction
+print("x * y = ", x * y)     # multiplication
+print("x / y = ", x / y)     # division
+print("x^y = ", x ** y)      # power
+print("remainder when x divides y = ", x % y)     # modulus
+print("x // y = ", x // y)     # floor division
+
